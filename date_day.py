b03d52329f62dbd34bc2a71d8730710bc85f6a8e
KshitishkumarPothal <kshitishkumarpothal@gmail.com>
2025-06-19 14:00:21 +0530

    recent practice file

---
+++ date_day.py
@@ -0,0 +1,9 @@
+# You are given a date. Your task is to find what the day is on that date.
+# Input
+# A single line of input containing the space separated month, day and year, respectively, in MM DD YYYY format.
+
+
+# 08 05 2015
+
+import datetime
+
